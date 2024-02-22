96186e853ecb338a41c6a030c61f016023055599
Dennis Gordeev <dennisgordeev@MacBook-Pro-von-Dennis.local>
2024-02-22 16:25:30 +0100

    search function & tile template

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,3 +6,22 @@ function toggleMenu() {
 
     sidebar.style.left = targetLeft + 'px';
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.getElementById('searchInput');
+    var sidebarItems = document.querySelectorAll('.sidebar ul li');
+
+    searchInput.addEventListener('input', function () {
+        var searchTerm = searchInput.value.toLowerCase();
+
+        sidebarItems.forEach(function (item) {
+            var itemText = item.textContent.toLowerCase();
+
+            if(itemText.includes(searchTerm)) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    });
+});
