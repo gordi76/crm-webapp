04be8d94a5e8763fe56754d6d29dd89fea6b1512
Dennis Gordeev <d.gordeev@polytec.de>
2024-04-23 16:25:24 +0200

    - added empty html files for switching between sites - added get method from database - added lead list table with search function in leads.html - overall style edits

--- public/db_connect.php
+++ public/db_connect.php
@@ -6,30 +6,13 @@ $password = "crmdashboard";
 $dbname = "crm_database";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-echo "connected ";
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = ('SELECT * FROM crm_database.leads');
-//$sql = ("INSERT INTO crm_database.leads (first_name, last_name, email, phone, company) values ('Dennis', 'Gordeev', 'dennis@example.com', '+49 176 1234567', 'ABC Inc.');");
 $result = $conn->query($sql);
-/*
-if($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-*/
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Name: " . $row["first_name"] . "<br>";
-    }
-} else {
-    echo "Keine Ergebnisse gefunden";
-}
 
 $conn->close();
 //$conn->set_charset("utf8");
